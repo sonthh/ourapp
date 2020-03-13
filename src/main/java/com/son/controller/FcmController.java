e8bfb24d259b67e84355f432297a44885de32c75
sonthh <tranhuuhongson@gmail.com>
2020-03-13 11:15:55 +0700

    init project

--- src/main/java/com/son/controller/FcmController.java
+++ src/main/java/com/son/controller/FcmController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @Controller
 @RequestMapping()
 @Validated
