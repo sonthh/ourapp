6a48d45e04529ea4d3bcd355bfc7fdf011813b0d
sonthh <tranhuuhongson@gmail.com>
2020-03-14 12:09:38 +0700

    checkCI

---
+++ src/test/java/com/son/config/SampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.son.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleUnitTest {
+
+    @Test
+    void sample() {
+        assertTrue(false);
+    }
+}
